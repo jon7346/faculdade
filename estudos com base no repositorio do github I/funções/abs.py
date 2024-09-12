49089d011de260e3eeaed63b2489a88febbe3783
jon7346 <jonathanfelipe7346@gmail.com>
2024-09-12 15:01:44 -0300

    s
    s

--- estudos com base no repositorio do github I/funções/abs.py
+++
@@ -1,7 +0,0 @@
-abs()
-'''''
-Retorna o valor absoluto de um número.
-O argumento pode ser um inteiro, um número de ponto flutuante ou um objeto implementando
-__abs__().
-Se o argumento é um número complexo, sua magnitude é retornada.
-'''''
--- estudos com base no repositorio do github I/funções/aiter.py
+++
@@ -1,3 +0,0 @@
-aiter()
-#Retorna um iterador assíncrono para um iterável assíncrono. Equivalente a chamar
-'Nota: Ao contrário de iter(), aiter() não tem uma variante de 2 argumentos.'

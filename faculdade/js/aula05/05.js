f3c4defc8bda5ce70cf7d76de8abf1db9db15ac0
jonathan <jonathanfelipe7346@gmail.com>
2024-10-02 20:49:35 -0300

    s

--- faculdade/js/aula05/05.js
+++ faculdade/js/aula05/05.js
@@ -1,1 +1,10 @@
+function listar(){
+    var numero = 0;
+    let lista = []
+   numero = document.getElementById("numero");
 
+   for (let i = 1 ; i < parseInt(numero) ; ++i){
+    lista.push(i)
+   }
+   alert(lista.join(' ,'))
+}
